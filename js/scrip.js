85b052bede8e976eb957073167a0e3c43345a78f
TyNguyen2001 <nguyenvantybg01@gmail.com>
2020-12-22 16:44:25 +0700

    12/22 new update

--- js/scrip.js
+++ js/scrip.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-    $(".fixed").waypoint(
-        function (direction) {
-            if (direction == "down") {
-                $("section").addClass("test");
-            } else {
-                $("section").removeClass("test");
-            }
-        },
-        {
-            offset: "0px",
-        }
-    );
-});
+// $(document).ready(function () {
+//     $(".fixed").waypoint(
+//         function (direction) {
+//             if (direction == "down") {
+//                 $("section").addClass("test");
+//             } else {
+//                 $("section").removeClass("test");
+//             }
+//         },
+//         {
+//             offset: "0px",
+//         }
+//     );
+// });
